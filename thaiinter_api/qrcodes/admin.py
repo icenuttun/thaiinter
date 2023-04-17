04913482806b96146276e44192f0f64920f9ed52
icenuttun <icenuttun@gmail.com>
2023-04-17 09:19:08 +0000

    update: add admin page for all models

--- thaiinter_api/qrcodes/admin.py
+++ thaiinter_api/qrcodes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import MetalSheetRoof, RoofType, RoofColor, RoofSize
 
 # Register your models here.
+admin.site.register(MetalSheetRoof)
+admin.site.register(RoofType)
+admin.site.register(RoofColor)
+admin.site.register(RoofSize)
--- thaiinter_api/tracking/admin.py
+++ thaiinter_api/tracking/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import RoofTracking, ProcessName
 
 # Register your models here.
+admin.site.register(RoofTracking)
+admin.site.register(ProcessName)
